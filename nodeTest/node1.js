1dbc330397ca5996a217e02727b38ef845184df7
liuheng kf10394 (CW <KF.liuhengA@h3c.com>
2023-12-04 18:00:22 +0800

    node2

--- nodeTest/node1.js
+++ nodeTest/node1.js
@@ -1,6 +1,9 @@
 let path = require("path");
-let demo = "./src/a/b/c.js"
+let demo = "C:/src/a/b/c.js"
 // console.log(path.basename(demo));
 // console.log(path.dirname(demo));
-
-console.log(path.parse(demo));
+// console.log(path.parse(demo));
+// console.log(path.resolve(__dirname, "./a"));
+//  console.log(path.resolve("../", "./a"));
+console.log(process.argv);
+console.log(process.execArgv);
